aca4f01e10e25519de01d1c1815c8d01d1a9866f
sandeshgajare55 <sandeshgajare35@gmail.com>
2024-07-11 14:05:23 +0530

    Ev Demo

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react'
+import Background from './components/Background/Background.jsx';
+import { Navbar } from './components/Navbar/Navbar.jsx';
+import { Hero } from './components/Hero/Hero.jsx';
+export const App = () => {
+
+  let heroData=[
+    {text1:"Dive Into",text2:"Speed"},
+    {text1:"Indulge",text2:"your Passions"},
+    {text1:"Give In to",text2:"Your Life"}
+  ]
+
+  const [heroCount,setHeroCount]= useState(0); // set the hero count 
+  const [playStatus,setPlayStatus]=useState(false); // set the video which is playing 
+
+  useEffect(()=>{
+    setInterval(() => {
+      setHeroCount((count)=>{return count===2 ? 0 : count+1});
+    }, 3500);
+  },[])
+  
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar/>
+      <Hero
+        setPlayStatus={setPlayStatus} //to play/pause video
+        heroData={heroData[heroCount]} // to render the text above
+        heroCount={heroCount}// take the count 
+        setHeroCount={setHeroCount}// to set image accordingly
+        playStatus={playStatus}
+      />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,31 @@
+import './Hero.css';
+import arrow_btn from '../../assets/arrow_btn.png';
+import play_icon from '../../assets/play_icon.png';
+import pause_icon from'../../assets/pause_icon.png';
+
+//we have call setHeroCount to return the values 012
+export const Hero = ({heroData,setHeroCount,heroCount,setPlayStatus,playStatus}) => {
+  return (
+    <div className='hero'>
+      <div className="hero-text">
+        <p>{heroData.text1}</p>
+        <p>{heroData.text2}</p>
+      </div>
+      <div className="hero-explore">
+        <p>Explore The Features..</p>
+        <img src={arrow_btn} alt=""></img>
+      </div>
+      <div className="hero-dot-play">
+        <ul className='hero-dots'>
+          <li className={heroCount===0? "hero-dot orange" : "hero-dot"} onClick={()=>setHeroCount(0)}></li>
+          <li  className={heroCount===1? "hero-dot orange" : "hero-dot"}onClick={()=>setHeroCount(1)}></li>
+          <li className={heroCount===2? "hero-dot orange" : "hero-dot"} onClick={()=>setHeroCount(2)}></li>
+        </ul>
+        <div className="hero-play">
+          <img src={playStatus? pause_icon : play_icon} onClick={()=>setPlayStatus(!playStatus)} alt=""></img>
+          <p>See the Video</p>
+        </div>
+      </div>
+    </div>
+  )
+}
