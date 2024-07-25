2b146455846cef597260a9ef13c37f46fe8e8be0
sandeshgajare55 <sandeshgajare35@gmail.com>
2024-07-25 14:52:02 +0530

    Added link to 4 menus

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,40 @@
-import React, { useEffect, useState } from 'react'
-import Background from './components/Background/Background.jsx';
-import { Navbar } from './components/Navbar/Navbar.jsx';
-import { Hero } from './components/Hero/Hero.jsx';
+import React, { useEffect, useState } from "react";
+import Background from "./components/Background/Background.jsx";
+import { Navbar } from "./components/Navbar/Navbar.jsx";
+import { Hero } from "./components/Hero/Hero.jsx";
+import { Home } from "./components/Home/Home.jsx";
 export const App = () => {
+  let heroData = [
+    { text1: "Dive Into", text2: "Speed" },
+    { text1: "Indulge", text2: "your Passions" },
+    { text1: "Give In to", text2: "Your Life" },
+  ];
 
-  let heroData=[
-    {text1:"Dive Into",text2:"Speed"},
-    {text1:"Indulge",text2:"your Passions"},
-    {text1:"Give In to",text2:"Your Life"}
-  ]
+  const [heroCount, setHeroCount] = useState(0); // set the hero count
+  const [playStatus, setPlayStatus] = useState(false); // set the video which is playing
 
-  const [heroCount,setHeroCount]= useState(0); // set the hero count 
-  const [playStatus,setPlayStatus]=useState(false); // set the video which is playing 
-
-  useEffect(()=>{
+  useEffect(() => {
     setInterval(() => {
-      setHeroCount((count)=>{return count===2 ? 0 : count+1});
+      setHeroCount((count) => {
+        return count === 2 ? 0 : count + 1;
+      });
     }, 3500);
-  },[])
-  
+  }, []);
+
   return (
     <div>
-      <Background playStatus={playStatus} heroCount={heroCount}/>
-      <Navbar/>
+      <Home />
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar />
       <Hero
         setPlayStatus={setPlayStatus} //to play/pause video
         heroData={heroData[heroCount]} // to render the text above
-        heroCount={heroCount}// take the count 
-        setHeroCount={setHeroCount}// to set image accordingly
+        heroCount={heroCount} // take the count
+        setHeroCount={setHeroCount} // to set image accordingly
         playStatus={playStatus}
       />
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,25 @@
-import './Navbar.css'
+import { Home } from "../Home/Home";
+import "./Navbar.css";
 
-// created a navbar which will display above 
+// created a navbar which will display above
 export const Navbar = () => {
   return (
-    <div className='nav'>
-      <div className='nav-logo'>EV Landing</div>
-      <ul className='nav-menu'>
-        <li>Home</li>
-        <li>Explore</li>
-        <li>About</li>
-        <li className='nav-contact'>Contact</li>
+    <div className="nav">
+      <div className="nav-logo">EV Landing</div>
+      <ul className="nav-menu">
+        <li className="nav-home">
+          <a href="#">Home</a>
+        </li>
+        <li className="nav-explore">
+          <a href="#">Explore</a>
+        </li>
+        <li className="nav-about">
+          <a href="#">About</a>
+        </li>
+        <li className="nav-contact">
+          <a href="#">Contact</a>
+        </li>
       </ul>
-
     </div>
-  )
-}
+  );
+};
