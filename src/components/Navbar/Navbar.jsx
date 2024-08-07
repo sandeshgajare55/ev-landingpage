ae5dd20e4a41d9a19e964e1e8a46768362a60904
sandeshgajare55 <sandeshgajare35@gmail.com>
2024-08-07 11:43:10 +0530

    Minor css changes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
-import Home  from "../Home/Home";
+import Home from "../Home/Home";
 import "./Navbar.css";
-import {BrowserRouter as Router,Route,Routes,Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import About from "../About/About";
 import Contact from "../Contact/Contact";
 import Explore from "../Explore/Explore";
@@ -8,31 +8,38 @@ import Explore from "../Explore/Explore";
 export const Navbar = () => {
   return (
     <Router>
-    <div className="nav">
-      <div className="nav-logo">EV Landing</div>
-      <ul className="nav-menu">
-        <li className="nav-home">
-          <Link to="/">Home</Link>
-        </li>
-        <li className="nav-explore">
-        <Link to="/explore">Explore</Link>
-        </li>
-        <li className="nav-about">
-        <Link to="/about">About</Link>
-        </li>
-        <li className="nav-contact">
-        <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </div>
+      <div className="nav">
+        <div className="nav-logo">EV Landing</div>
+        <ul className="nav-menu">
+          <li className="nav-home">
+            <Link className="nav-home" to="/">
+              Home
+            </Link>
+          </li>
+          <li className="nav-explore">
+            <Link className="nav-explore" to="/explore">
+              Explore
+            </Link>
+          </li>
+          <li className="nav-about">
+            <Link className="nav-about" to="/about">
+              About
+            </Link>
+          </li>
+          <li className="nav-contact">
+            <Link className="nav-contact" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
 
-
-    <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/explore" element={<Explore />} />
-    <Route path="/about" element={<About />} />
-    <Route path="/contact" element={<Contact />} />
-         </Routes>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </Router>
   );
 };
